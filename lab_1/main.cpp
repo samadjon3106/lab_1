4362a366c7bc25653261064f960688a2e9dd29ef
s.sayfullayev <s.sayfullayev@newuu.uz>
2025-01-17 09:32:14 +0500

    Initial Commit

---
+++ lab_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_1
+//
+//  Created by Samadjon Sayfullayev on 17/01/25.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i=0; i<10; i++){
+        cout << i<<" Hello, World!\n";
+    }
+    return 0;
+}
